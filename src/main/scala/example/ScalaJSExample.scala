23abe08739188a7cb99d18c5d183fa2bce008e10
bbarker <beb82@cornell.edu>
2015-09-08 12:18:34 -0400

    demonstrating possibly dubious usage of new js.Object

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -1,6 +1,7 @@
 package example
 
 import scala.scalajs.js
+import js.Dynamic.{literal => lit}
 import js.annotation.JSExport
 import org.scalajs.dom
 
@@ -15,4 +16,10 @@ object ScalaJSExample extends js.JSApp {
    *  This demonstrates unit testing.
    */
   def square(x: Int): Int = x*x
+
+  val squareJS: js.Function1[Int, Int] = {x: Int => x * x}
+
+  @JSExport
+  def getObj(): js.Object = new js.Object{val square = squareJS}
+  //def getObj(): js.Object = lit(square = squareJS)
 }
